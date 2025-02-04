da05a2bf8ca83cf74ced3d1504d375c6b77553a8
tima20 <trjjinva@gmail.com>
2025-02-04 16:34:54 -0500

    Changed MainPanel to use flexbox instead of grid

--- arkhamSealed/src/components/MainPanel.tsx
+++ arkhamSealed/src/components/MainPanel.tsx
@@ -2,29 +2,16 @@ import automatic45 from "../assets/45-automatic.png";
 
 const MainPanel = () => {
   return (
-    <div className="container">
-      <div className="row py-1">
-        <div className="col">
-          <img src={automatic45}></img>
-        </div>
-        <div className="col">
-          <img src={automatic45}></img>
-        </div>
-        <div className="col">
-          <img src={automatic45}></img>
-        </div>
-      </div>
-      <div className="row py-1">
-        <div className="col">
-          <img src={automatic45}></img>
-        </div>
-        <div className="col">
-          <img src={automatic45}></img>
-        </div>
-        <div className="col">
-          <img src={automatic45}></img>
-        </div>
-      </div>
+    <div className="container d-flex flex-wrap">
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
+      <img src={automatic45}></img>
     </div>
   );
 };
